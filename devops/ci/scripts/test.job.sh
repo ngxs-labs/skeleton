33ef15d78c84856166bfdea8f56e89d168754725
arturovt <arthurandrosovich@gmail.com>
2019-05-22 23:01:05 +0300

    fix: update test job script

--- devops/ci/scripts/test.job.sh
+++ devops/ci/scripts/test.job.sh
@@ -4,4 +4,4 @@ docker run \
   -v $PWD:/tmp/src \
   --name $APPVEYOR_JOB_ID \
   node:11 /bin/bash -c \
-  "mkdir /tmp/build && cp -a /tmp/src/. /tmp/build && cd /tmp/build && yarn --pure-lockfile --non-interactive --no-progress && yarn say-hello"
+  "mkdir /tmp/build && cp -a /tmp/src/. /tmp/build && cd /tmp/build && yarn --pure-lockfile --non-interactive --no-progress && yarn ci:pipelines"
