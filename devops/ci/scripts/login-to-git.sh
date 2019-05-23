a43abfe1e60947fc2a45b710c40e5585ab1e67e7
Artur Androsovych <arthurandrosovich@gmail.com>
2019-05-23 22:17:20 +0300

    feat: configure `bump-version` job (#3)

--- devops/ci/scripts/login-to-git.sh
+++ devops/ci/scripts/login-to-git.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote rm origin
+git remote add origin https://arturovt:${ACCESS_TOKEN}@github.com/arturovt/travis-test.git > /dev/null 2>&1
--- devops/definitions.sh
+++ devops/definitions.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 export NGXS_LABS_PROJECT_NAME=skeleton
+export GREEN="\e[92m"
