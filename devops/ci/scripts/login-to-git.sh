f37bf05882c99700aaacfab980a1dd984bd738b9
arturovt <arthurandrosovich@gmail.com>
2019-05-23 22:20:42 +0300

    fix: git remote

--- devops/ci/scripts/login-to-git.sh
+++ devops/ci/scripts/login-to-git.sh
@@ -3,4 +3,4 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote rm origin
-git remote add origin https://arturovt:${ACCESS_TOKEN}@github.com/arturovt/travis-test.git > /dev/null 2>&1
+git remote add origin https://arturovt:${ACCESS_TOKEN}@github.com/ngxs-labs/${NGXS_LABS_PROJECT_NAME}.git > /dev/null 2>&1
